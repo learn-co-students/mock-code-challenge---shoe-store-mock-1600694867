01113fd179c479da1b950a5248f996b6cd4cb43a
Alex E(She/Her) <eisea904@newschool.edu>
2020-09-21 11:05:55 -0400

    making review list items

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+const sidebarList = document.querySelector("#shoe-list")
+const shoeName = document.querySelector("#shoe-name")
+const shoeDesc = document.querySelector("#shoe-description")
+const shoePrice = document.querySelector("#shoe-price")
+const shoeForm = document.querySelector("#form-container")
+const shoeReviews = document.querySelector("#reviews-list")
+
+
+let turnShoeToHTML = (shoe) => {
+    shoeName.innerText = shoe.name
+    shoeDesc.innerText = shoe.description
+    shoePrice.innerText = shoe.price
+debugger 
+    shoe.reviews
+    // reviewLi = document.createElement("li")
+    // shoeReviews.append(reviewLi)
+    // shoeReviews.innerText = shoe.reviews 
+}
+
+fetch("http://localhost:3000/shoes")
+    .then(res => res.json())
+    .then(shoes => {
+        shoes.forEach(turnShoeToHTML)
+    })
