fef943d73d5b0486b9db17565f91c0d1bf2c901e
Alex E(She/Her) <eisea904@newschool.edu>
2020-09-21 12:26:21 -0400

    fixing form for new reviews

--- Frontend/index.js
+++ Frontend/index.js
@@ -4,21 +4,47 @@ const shoeImg = document.querySelector("#shoe-image")
 const shoeName = document.querySelector("#shoe-name")
 const shoeDesc = document.querySelector("#shoe-description")
 const shoePrice = document.querySelector("#shoe-price")
-const shoeForm = document.querySelector("#form-container")
+const formContainer = document.querySelector("#form-container")
 const shoeReviews = document.querySelector("#reviews-list")
 
 
 let turnShoeToHTML = (shoe) => {
     shoeImg.src = shoe.image
-    shoeImg.alt = shoe.name 
+        shoeImg.alt = shoe.name 
     shoeName.innerText = shoe.name
     shoeDesc.innerText = shoe.description
-    shoePrice.innerText = shoe.price
-    shoe.reviews.forEach(makeReviewLi)
-    // reviewLi = document.createElement("li")
-    // shoeReviews.append(reviewLi)
-    // shoeReviews.innerText = shoe.reviews 
+    shoePrice.innerText = `$${shoe.price} USD`
+    shoe.reviews.forEach(makeReviewLi)              // Populate ul with each review li
+
+    let reviewForm = document.createElement("form")     // Create form to submit new review
+        reviewForm.method = 'POST'
+        let formElement1 = document.createElement('input')
+        formElement1.setAttribute('type', 'text')
+        reviewForm.append(formElement1)
+        formContainer.append(reviewForm)        // Append form inside 
+
+    let reviewButton = document.createElement('button') // Make Button to Submit New Review
+        reviewButton.innerText = 'Post Review'
+        formContainer.append(reviewButton)
+        reviewButton.addEventListener("click", (evt) => {reviewForm.submit()})
+
+    // shoeForm.addEventListener("submit", (evt) => {      // Event listener to be triggered when reviewButton "click" triggers "submit"
+    //     debugger
+    //     evt.preventDefault()
+    //     fetch(`http://localhost:3000/shoes/${shoe.id}`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             reviews: 
+
+    //         })
+    //     })
+    // })
 }
+
 
 fetch("http://localhost:3000/shoes")
     .then(res => res.json())
@@ -26,6 +52,8 @@ fetch("http://localhost:3000/shoes")
         shoes.forEach(turnShoeToHTML)
     })
 
+
+// Make Review list item
 let makeReviewLi = (review) => {
     reviewLi = document.createElement("li")
     reviewLi.innerText = `"${review.content}"`
