9e76cfee0d6ab3d6b73e19bc663d622298967a56
Alex E(She/Her) <eisea904@newschool.edu>
2020-09-21 11:20:48 -0400

    first deliverable

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,6 @@
 // Code your solution here
 const sidebarList = document.querySelector("#shoe-list")
+const shoeImg = document.querySelector("#shoe-image")
 const shoeName = document.querySelector("#shoe-name")
 const shoeDesc = document.querySelector("#shoe-description")
 const shoePrice = document.querySelector("#shoe-price")
@@ -8,11 +9,12 @@ const shoeReviews = document.querySelector("#reviews-list")
 
 
 let turnShoeToHTML = (shoe) => {
+    shoeImg.src = shoe.image
+    shoeImg.alt = shoe.name 
     shoeName.innerText = shoe.name
     shoeDesc.innerText = shoe.description
     shoePrice.innerText = shoe.price
-debugger 
-    shoe.reviews
+    shoe.reviews.forEach(makeReviewLi)
     // reviewLi = document.createElement("li")
     // shoeReviews.append(reviewLi)
     // shoeReviews.innerText = shoe.reviews 
@@ -23,3 +25,9 @@ fetch("http://localhost:3000/shoes")
     .then(shoes => {
         shoes.forEach(turnShoeToHTML)
     })
+
+let makeReviewLi = (review) => {
+    reviewLi = document.createElement("li")
+    reviewLi.innerText = `"${review.content}"`
+    shoeReviews.append(reviewLi)
+}
